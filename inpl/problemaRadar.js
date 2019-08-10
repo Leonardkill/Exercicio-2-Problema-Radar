990c837aaae6f6136c2cc4f4c1473a21b06b8fba
Jacob <leonardofaria_7@hotmail.com>
2019-08-09 22:24:38 -0300

    Subindo Function

--- inpl/problemaRadar.js
+++ inpl/problemaRadar.js
@@ -1,14 +1,16 @@
 var freqcarro = 0.7;
 var velcarro;
-var constante = 300;
 var fradar = 120;
 var multa;
-velcarro = 300 - (fradar / freqcarro - 1);
-if (velcarro < 144) {
-    console.log("Multa de : R$127,69");
-    console.log(velcarro);
-}
-else {
-    console.log("Multa de : R$574,62");
-    console.log(velcarro);
-}
+var calcvelcarro = function (radar, carro) {
+    velcarro = 300 - (fradar / freqcarro - 1);
+    if (velcarro < 144) {
+        console.log("Multa de : R$127,69");
+        console.log("A velocidade do carro é" + velcarro);
+    }
+    else {
+        console.log("Multa de : R$574,62");
+        console.log("A velocidade do carro é : " + velcarro);
+    }
+};
+calcvelcarro(fradar, freqcarro);
